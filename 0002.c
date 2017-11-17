2f30dd65618300308fc6a1af1692e36eb5cc68a9
John Hannemann <Hondough@users.noreply.github.com>
2017-11-16 22:35:58 -0600

    corrected file type to c from cpp

---
+++ 0002.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void)
+{
+    printf ("Jamsa\'s C/C++/C# Programmer\'s Bible");
+}
--- 0002.cpp
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-void main(void)
-{
-    printf "Jamsa\'s C/C++/C# Programmer\'s Bible");
-}
