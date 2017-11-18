03418ad6f20b7afe36fde3dcd737e7e4581fe547
John Hannemann <Hondough@users.noreply.github.com>
2017-11-18 16:48:29 -0600

    Commenting programs

---
+++ 0016-comment.c
@@ -0,0 +1,11 @@
+// Program: 0016-comment.c
+// Written by: John Hannemann
+// Date written: 18-Nov-17
+// Purpose: Example of comments in a C program
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Jamsa\'s C/C++/C# Programmer\'s Bible!"); // Display a message
+}
