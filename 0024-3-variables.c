832a2b5d8cd3c41da5e780f755b19913435cd06d
John Hannemann <Hondough@users.noreply.github.com>
2017-11-18 21:07:25 -0600

    Assigning a value to a variable

---
+++ 0024-3-variables.c
@@ -0,0 +1,10 @@
+int main(void)
+{
+    int age;      // the user's age in years
+    int weight;   // the user's weight in pounds
+    int height;   // the user's height in inches
+
+    age = 41;     // assign the user's age
+    weight = 165; // assign the user's weight
+    height = 73;  // assign the user's height
+}
