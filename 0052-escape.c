6c339e62f3cfb956c934d2c0034df36666e3c99f
John Hannemann <Hondough@users.noreply.github.com>
2017-11-27 22:33:15 -0600

    Assigning quotes and other characters

---
+++ 0052-escape.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char single_quote = '\'';
+    char double_quote = '\"';
+
+    char ascii_bell = '\a';
+    char backspace = '\b';
+    char formfeed = '\f';
+    char newline = '\n';
+}
