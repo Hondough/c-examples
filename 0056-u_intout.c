0eb69aa0acc087e819a915a1717401ab9c1ce077
John Hannemann <Hondough@users.noreply.github.com>
2017-11-28 22:09:45 -0600

    Displaying values of type unsigned int using printf

---
+++ 0056-u_intout.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned int value = 4000000000;
+
+    printf("Displaying 4000000000 as unsigned %u\n", value);
+    printf("Displaying 4000000000 as in %d\n", value);
+}
